a1658367718fa78fe9fc4035c13600ad2af723af
Bingor <xiaobinbingor@163.com>
2018-11-16 18:34:16 +0800

    1

---
+++ TestProject/js/index.js
@@ -0,0 +1,9 @@
+// {/* <script src="https://cdn.jsdelivr.net/npm/vue"></script> */}
+
+Vue.component('my_li', {
+    template: '<li>ndfjgsjgr</li>'
+})
+
+var test_template1 = new Vue({
+    el: '#div_test_template1'
+})
